6808e815cb11e9617f09237ba9c9b3ca4d67cf8f
Vinax89 <migz89star@gmail.com>
2025-08-26 21:27:04 -0700

    feat: add safe notify function

--- src/utils/notifications.ts
+++ src/utils/notifications.ts
@@ -4,3 +4,16 @@ export async function requestNotificationPermission() {
     await Notification.requestPermission();
   }
 }
+
+export async function notify(title: string, options?: NotificationOptions) {
+  if (!('Notification' in window)) return;
+
+  if (Notification.permission === 'denied') return;
+
+  if (Notification.permission === 'default') {
+    const permission = await Notification.requestPermission();
+    if (permission !== 'granted') return;
+  }
+
+  new Notification(title, options);
+}
