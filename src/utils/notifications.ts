582d19c9f7ad4b8d2ce8e4de0135359ca6d6b3ee
Vinax89 <migz89star@gmail.com>
2025-08-26 21:18:09 -0700

    Check notification permission before requesting

---
+++ src/utils/notifications.ts
@@ -0,0 +1,6 @@
+export async function requestNotificationPermission() {
+  if (!('Notification' in window)) return;
+  if (Notification.permission === 'default') {
+    await Notification.requestPermission();
+  }
+}
